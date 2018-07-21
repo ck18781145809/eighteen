28b189ba1c65b09a1678bb0562296c717ad4d048
Kuo Chen <18781145809@163.com>
2018-07-21 11:45:36 +0800

    add gulp-livereload
    添加 gulp-livereload插件

--- Gulp/Tradition/gulpfile.js
+++ Gulp/Tradition/gulpfile.js
@@ -8,19 +8,20 @@ var gulp         = require( 'gulp' ),
 	concat       = require( "gulp-concat" ), //  合并文件
 	replace      = require( 'gulp-replace' ), //  字符串替换
 	babel        = require( "gulp-babel" ), //  es6转译
+	livereload   = require('gulp-livereload'); //  chrome实时刷新
 	base64       = require('gulp-base64'); //  img转base64
 
 //  每次执行先删除旧的dist 然后再生成一个新的dist
-gulp.task( 'default', [ 'clean' ], function() {
+gulp.task( 'default', [ 'clean', 'watch' ], function() {
 	gulp.start( 'img' );
-} )
+} );
 
 //  开发环境的index输出到发布包中
 gulp.task( 'html', function() {
 	return gulp .src( './src/*.html' )
-				.pipe( minifyHtml() )
-				.pipe( gulp.dest( './dist/' ) )
-} )
+		.pipe( minifyHtml() )
+		.pipe( gulp.dest( './dist/' ) )
+} );
 
 //	对css文件加md5控制版本
 gulp.task( 'css', [ 'html' ], function() {
@@ -41,34 +42,38 @@ gulp.task( 'css', [ 'html' ], function() {
 					mappingFile: 'manifest.json' //  将对应关系写到mainfest.json中
 				} ) )
 				.pipe( gulp.dest( "./dist/css/" ) );
-} )
+} );
 
 //	对js文件加md5控制版本
 gulp.task( 'js', [ 'css' ], function() {
 	return gulp .src( "./src/js/*.js" )
 				.pipe( babel() )
-				.pipe( uglify() )
 				.pipe( md5( 10, './dist/*.html', {
 					mappingFile: 'manifest.json' //  将对应关系写到mainfest.json中
 				} ) )
 				.pipe( gulp.dest( "./dist/js/" ) );
-} )
+} );
 
 //  输出字体文件到dist
 gulp.task( 'font', [ 'js' ], function() {
 	return gulp .src( "./src/font/**" )
 				.pipe( gulp.dest( "./dist/font" ) );
-} )
+} );
 
 //	输出图片到dist
 gulp.task( 'img', [ 'font' ], function() {
 	return gulp .src( "./src/images/**" )
-			    .pipe( gulp.dest( "./dist/images/" ) );
-} )
+			    .pipe( gulp.dest( "./dist/images/" ) )
+				.pipe( livereload() )
+} );
 
 //  删除发布包和mainfest.json
 gulp.task( 'clean', function() {
 	return del( [ './dist', './manifest.json' ] );
 } );
+
+gulp.task('watch', function() {
+	livereload.listen(); //要在这里调用listen()方法
+});
 
 gulp.watch( './src/**', [ 'default' ] );
